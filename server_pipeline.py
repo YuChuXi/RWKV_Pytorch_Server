f4a762f60d11be62e76009b9c729207341d4f3bf
YuChuXi <cyu245656@gmail.com>
2024-07-18 18:52:51 +0800

    fix l 124

--- server_pipeline.py
+++ server_pipeline.py
@@ -121,7 +121,7 @@ class RWKVState:
             )
             print({
                     "processed_tokens": self.processed_tokens,
-                    "logits": self.logits.grad(),
+                    "logits": self.logits.grad,
                     "processed_tokens_counts": self.processed_tokens_counts,
                 })
         # np.save(f"data/{state_name}/state.npy", (np.arcsinh(self.state) * 24).clip(-128,127).astype(np.int8))
